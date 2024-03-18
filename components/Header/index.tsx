81b4a02ac072144ad21aef3dbae10a38e720a050
Vadimcha <BelovBadim2014@yandex.ru>
2024-03-18 11:25:51 +0300

    Make pages title

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Avatar, Divider, Flex, Group} from "@mantine/core";
 import styles from './Header.module.scss'
+import {NavLinks} from "@/components/NavBar/NavLinks";
 
 export const Header = ({ children }: {children: React.ReactNode}) => {
+    const [label, setLabel] = useState<string>('Ошибка')
+    useEffect(() => {
+        const curLink = NavLinks.find(link => link.src == window.location.pathname)
+        if(curLink != undefined)
+            setLabel(curLink.label)
+    }, [children])
     return (
         <div>
             <Flex
@@ -11,7 +18,9 @@ export const Header = ({ children }: {children: React.ReactNode}) => {
                 justify={"space-between"}
             >
                 {children}
-                <h2 className={styles.title}>Страница с привычками</h2>
+                <h2 className={styles.title}>
+                    { label }
+                </h2>
                 <Group>
                     <Avatar radius={"xl"} size={"30"} alt={"Avatar"} />
                 </Group>
